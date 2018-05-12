082c0f47646f2ffb898e20551c8415319af998ff
Raul de Araújo <rauldearaujolima@gmail.com>
2018-05-12 08:26:20 -0300

    Correção na forma de concatenar textos de um JSON tweet

--- Automatic Classification/AutomaticClassifier.py
+++ Automatic Classification/AutomaticClassifier.py
@@ -18,23 +18,23 @@ def main(candidato,jsonFileNames):
 			simple_tweet['user_screen_name'] = tweet['user']['screen_name']
 			simple_tweet['user_name'] = tweet['user']['name']
 			text_aux = ''
-			if 'is_quoted_status' in tweet:
+            # Get initial text
+            if 'extended_tweet' in tweet:
+				text_aux = tweet['extended_tweet']['full_text']
+			else:
+                text_aux = tweet['text']
+            # If is quoted_status, append the text to variable
+            if 'is_quoted_status' in tweet:
 				if tweet['is_quoted_status'] == 'true':
 					if 'extended_tweet' in tweet['quoted_status']:
-						text_aux = tweet['text'] + tweet['quoted_status']['extended_tweet']['full_text']
+						text_aux += '\n>>>QUOTED>>>\n' + tweet['text'] + tweet['quoted_status']['extended_tweet']['full_text']
 					else:
 						text_aux = tweet['text'] + tweet['quoted_status']['text']
 			elif 'retweeted_status' in tweet:
 				if 'extended_tweet' in tweet['retweeted_status']:
-					text_aux = tweet['retweeted_status']['extended_tweet']['full_text']
+					text_aux += '\n>>>RETWEETED>>>\n' + tweet['retweeted_status']['extended_tweet']['full_text']
 				else:
 					text_aux = tweet['retweeted_status']['text']
-			else:
-				if 'extended_tweet' in tweet:
-					text_aux = tweet['extended_tweet']['full_text']
-				else:
-					text_aux = tweet['text']
-
 			simple_tweet['full_text'] = text_aux.replace(';','').replace('\n', ' ')
 			simple_tweet['emot_class'] = classByEmoji(simple_tweet['full_text'])
 			simple_tweet['semantic_class'] = termScoreSummationMethod(preProcessing(simple_tweet['full_text']))
